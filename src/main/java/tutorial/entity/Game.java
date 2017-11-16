f938a96199aec6fda73ae1c3a115865ff838fc45
kidcodely <kidcodely@gmail.com>
2017-11-15 19:53:35 -0800

    Tutorial 3: Mapping Java classes to the database

---
+++ src/main/java/tutorial/entity/Game.java
@@ -0,0 +1,64 @@
+package tutorial.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "game")
+public class Game {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column(name = "title", nullable = false)
+	private String title;
+	
+	@Column(name = "description")
+	private String description;
+	
+	@Column(name = "release_date")
+	private Date releaseDate;
+	
+	public Game(){}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(Date releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	
+	
+}
